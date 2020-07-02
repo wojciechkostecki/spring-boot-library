2d3e43e27fdf8549a4db6bbcfd0014510b45ec23
Wojciech Kostecki <wt.kostecki@gmail.com>
2020-07-02 21:23:59 +0200

    adding global exception handler

---
+++ src/main/java/pl/wojciechkostecki/library/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package pl.wojciechkostecki.library.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.persistence.EntityNotFoundException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {EntityNotFoundException.class})
+    protected ResponseEntity<Object> handleNotFound(RuntimeException ex, WebRequest request){
+        return handleExceptionInternal(ex, ex.getMessage(),new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+
+    @ExceptionHandler(value = {IllegalArgumentException.class})
+    protected ResponseEntity<Object> handleBadRequest(RuntimeException ex, WebRequest request){
+        return handleExceptionInternal(ex, ex.getMessage(),new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+}
