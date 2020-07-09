e94099cfb0cf40539bce227a33646835d55a1fbb
Wojciech Kostecki <wt.kostecki@gmail.com>
2020-07-09 19:21:55 +0200

    Adding logging in BookController

--- src/main/java/pl/wojciechkostecki/library/controller/BookController.java
+++ src/main/java/pl/wojciechkostecki/library/controller/BookController.java
@@ -1,5 +1,7 @@
 package pl.wojciechkostecki.library.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,7 @@ import java.util.List;
 @RequestMapping("/api/books")
 public class BookController {
 
+    private final Logger logger = LoggerFactory.getLogger(BookController.class);
     private final BookService bookService;
 
     public BookController(BookService bookService) {
@@ -20,27 +23,32 @@ public class BookController {
 
     @PostMapping
     public ResponseEntity<Book> createBook(@RequestBody Book book) {
+        logger.debug("REST request to create Book: {}",book);
         Book savedBook = bookService.save(book);
         return new ResponseEntity<>(savedBook, HttpStatus.CREATED);
     }
 
     @GetMapping
     public ResponseEntity<List<Book>> getAllBooks(){
+        logger.debug("REST request to get all Books");
         return ResponseEntity.ok(bookService.getAll());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Book> getBook(@PathVariable Long id){
+        logger.debug("REST request to get Book: {}",id);
         return ResponseEntity.ok(bookService.findById(id));
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book book){
+        logger.debug("REST request to update Book: {} with id {}",book,id);
         return ResponseEntity.ok(bookService.updateBook(id,book));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteBook(@PathVariable Long id){
+        logger.debug("REST request to delete Book: {}",id);
         bookService.delete(id);
         return ResponseEntity.noContent().build();
     }
