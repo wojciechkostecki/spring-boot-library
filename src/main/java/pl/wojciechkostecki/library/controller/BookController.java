026d6dc4091c1bb5278adfe31ada0be71231405f
Wojciech Kostecki <wt.kostecki@gmail.com>
2020-07-19 19:58:18 +0200

    Adding security. Adding homecontroller

--- src/main/java/pl/wojciechkostecki/library/controller/BookController.java
+++ src/main/java/pl/wojciechkostecki/library/controller/BookController.java
@@ -53,4 +53,6 @@ public class BookController {
         return ResponseEntity.noContent().build();
     }
 
+
+
 }
---
+++ src/main/java/pl/wojciechkostecki/library/security/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package pl.wojciechkostecki.library.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers( "/home").permitAll()
+                .anyRequest().authenticated();
+
+    }
+}
