f4d6023c9e9b3d8acb90986031135da55088338b
Wojciech Kostecki <wt.kostecki@gmail.com>
2020-07-23 20:19:35 +0200

    Adding login and logout endpoints

--- src/main/java/pl/wojciechkostecki/library/security/WebSecurityConfig.java
+++ src/main/java/pl/wojciechkostecki/library/security/WebSecurityConfig.java
@@ -12,8 +12,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers( "/home").permitAll()
-                .anyRequest().authenticated();
-
+                .antMatchers("/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
     }
 }
