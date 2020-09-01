9509151a400815a111d96e49a2f1dfaa36b62cf3
Wojciech Kostecki <wt.kostecki@gmail.com>
2020-09-01 18:51:04 +0200

    Adding admin on app startup

--- src/main/java/pl/wojciechkostecki/library/LibraryApplication.java
+++ src/main/java/pl/wojciechkostecki/library/LibraryApplication.java
@@ -5,20 +5,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import pl.wojciechkostecki.library.model.Role;
+import pl.wojciechkostecki.library.model.User;
 import pl.wojciechkostecki.library.model.UserRole;
 import pl.wojciechkostecki.library.repository.UserRepository;
 import pl.wojciechkostecki.library.service.UserRoleService;
+import pl.wojciechkostecki.library.service.UserService;
 
 @SpringBootApplication
 public class LibraryApplication {
 
     private final UserRoleService userRoleService;
 
+    private final UserService userService;
+
     private final UserRepository userRepository;
 
-    public LibraryApplication(UserRepository userRepository, UserRoleService userRoleService) {
+    public LibraryApplication(UserRepository userRepository, UserRoleService userRoleService, UserService userService) {
         this.userRepository = userRepository;
         this.userRoleService = userRoleService;
+        this.userService = userService;
     }
 
     public static void main(String[] args) {
@@ -29,6 +34,14 @@ public class LibraryApplication {
     @EventListener(ApplicationReadyEvent.class)
     public void afterInitSetup() {
         saveRoles();
+        saveUsers();
+    }
+
+    private void saveUsers() {
+        User user = new User();
+        user.setUsername("admin");
+        user.setPassword("admin");
+        userService.registerUser(user);
     }
 
     private void saveRoles() {
